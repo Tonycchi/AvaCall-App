7de8f933e3283373fbb91769f39d62fb89cd5ffc
benedikt.mehl <benedikt.mehl@stud.tu-darmstadt.de>
2021-02-20 14:29:43 +0100

    trimmedURL geändert

--- app/src/main/java/com/example/rcvc/TrimmedURL.java
+++ app/src/main/java/com/example/rcvc/TrimmedURL.java
@@ -5,14 +5,22 @@ import java.net.MalformedURLException;
 
 public class TrimmedURL {
 
-    public String url;
+    private String hostname;
 
     public TrimmedURL(String url) throws MalformedURLException {
-        this.url = url;
+        this.hostname = hostname;
         if (!Patterns.WEB_URL.matcher(url).matches()) {
             throw new MalformedURLException();
         }
-        this.url = this.url.replaceFirst("^(http[s]?://www\\.|http[s]?://|www\\.)", "");
-        this.url = this.url.replaceFirst("/.*", "");
+        this.hostname = this.hostname.replaceFirst("^(http[s]?://www\\.|http[s]?://|www\\.)", "");
+        this.hostname = this.hostname.replaceFirst("/.*", "");
+    }
+
+    public String getUrl(){
+        return "https://"+hostname;
+    }
+
+    public String getHostname(){
+        return hostname;
     }
 }
