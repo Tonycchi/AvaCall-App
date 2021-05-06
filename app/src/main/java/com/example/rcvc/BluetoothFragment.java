f498436955b29737353ab83a90bf52985fe54f0e
BenediktMehl <b.mehl14@outlook.de>
2021-05-06 21:21:22 +0200

    added BluetoothFragment

---
+++ app/src/main/java/com/example/rcvc/BluetoothFragment.java
@@ -0,0 +1,8 @@
+package com.example.rcvc;
+
+import androidx.fragment.app.Fragment;
+
+public class BluetoothFragment extends Fragment {
+
+    
+}
--- app/src/main/java/com/example/rcvc/HostActivity.java
+++ app/src/main/java/com/example/rcvc/HostActivity.java
@@ -1,7 +1,16 @@
 package com.example.rcvc;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HostActivity extends AppCompatActivity {
-    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.bluetooth_title);
+        setContentView(R.layout.bluetooth_connection);
+    }
+
 }
