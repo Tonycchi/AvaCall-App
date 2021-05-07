e7a113a1a29ce031fc677b013e94f1700e8e741a
BenediktMehl <b.mehl14@outlook.de>
2021-05-07 14:33:19 +0200

    host activity now used the fragment and not the bluetooth_connection.xml directly

--- app/src/main/java/com/example/rcvc/BluetoothFragment.java
+++ app/src/main/java/com/example/rcvc/BluetoothFragment.java
@@ -1,8 +1,19 @@
 package com.example.rcvc;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class BluetoothFragment extends Fragment {
+    public BluetoothFragment() {
+        super(R.layout.bluetooth_connection);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+    }
 
 }
--- app/src/main/java/com/example/rcvc/HostActivity.java
+++ app/src/main/java/com/example/rcvc/HostActivity.java
@@ -6,11 +6,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class HostActivity extends AppCompatActivity {
 
+    public HostActivity() {
+        super(R.layout.host);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle(R.string.bluetooth_title);
-        setContentView(R.layout.bluetooth_connection);
+
+        if (savedInstanceState == null) { //In the previous example, note that the fragment transaction is only created when savedInstanceState is null. This is to ensure that the fragment is added only once, when the activity is first created. When a configuration change occurs and the activity is recreated, savedInstanceState is no longer null, and the fragment does not need to be added a second time, as the fragment is automatically restored from the savedInstanceState.
+            Bundle bundle = new Bundle(); //not needed yet, maybe later
+            bundle.putInt("some_int", 0); //TODO: delete if not used
+
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, BluetoothFragment.class, bundle)
+                    .commit();
+
+            setTitle(R.string.bluetooth_title);
+        }
     }
 
 }
