7353eb0fc639234663273833286288c5113b93c5
alexander.gottfried <alexander.gottfried@stud.tu-darmstadt.de>
2021-05-07 15:20:33 +0200

    edit_controls.xml portrait done

---
+++ app/src/main/java/com/example/rcvc/BaseFragment.java
@@ -0,0 +1,23 @@
+package com.example.rcvc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public abstract class BaseFragment extends Fragment {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanseState) {
+        View view = provideYourFragmentView(inflater,parent,savedInstanseState);
+        return view;
+    }
+
+    public abstract View provideYourFragmentView(LayoutInflater inflater,ViewGroup parent, Bundle savedInstanceState);
+
+}
