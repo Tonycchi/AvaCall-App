48f67b73ecfcf08901413ad871dd7195c6ab09e0
BenediktMehl <b.mehl14@outlook.de>
2021-05-07 14:40:41 +0200

    nighmode disabled

--- app/src/main/java/com/example/rcvc/HostActivity.java
+++ app/src/main/java/com/example/rcvc/HostActivity.java
@@ -3,6 +3,7 @@ package com.example.rcvc;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class HostActivity extends AppCompatActivity {
 
@@ -13,6 +14,10 @@ public class HostActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //disables nightmode even if nightmode is activated on the device
+        //TODO: implement nightmode and delete this line
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         if (savedInstanceState == null) { //In the previous example, note that the fragment transaction is only created when savedInstanceState is null. This is to ensure that the fragment is added only once, when the activity is first created. When a configuration change occurs and the activity is recreated, savedInstanceState is no longer null, and the fragment does not need to be added a second time, as the fragment is automatically restored from the savedInstanceState.
             Bundle bundle = new Bundle(); //not needed yet, maybe later
