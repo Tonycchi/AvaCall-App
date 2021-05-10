d7a387b387864bdcc9cf1f05b29e36566a437921
Tonycchi <tn1499@yahoo.de>
2021-05-10 13:46:05 +0200

    changed BaseFragment to EditControlFragment and comment out fragment in edit_controls.xml

---
+++ app/src/main/java/com/example/rcvc/EditEV3ControlFragment.java
@@ -0,0 +1,13 @@
+package com.example.rcvc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EditEV3ControlFragment extends EditModelControlFragment {
+    @Override
+    public View provideYourFragmentView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/rcvc/EditModelControlFragment.java
+++ app/src/main/java/com/example/rcvc/EditModelControlFragment.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-public abstract class BaseFragment extends Fragment {
+public abstract class EditModelControlFragment extends Fragment {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
