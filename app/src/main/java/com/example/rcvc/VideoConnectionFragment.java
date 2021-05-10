eff9fdc9861c4fa7c98e1d470c397189ceb12de6
LauritzR <rasbachlauritz@googlemail.com>
2021-05-10 13:50:50 +0200

    Implemented VideoConnectionFragment

--- app/src/main/java/com/example/rcvc/VideoConnectionFragment.java
+++ app/src/main/java/com/example/rcvc/VideoConnectionFragment.java
@@ -1,11 +1,44 @@
 package com.example.rcvc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class VideoConnectionFragment extends Fragment {
+
+    public VideoConnectionFragment() {
+        super(R.layout.video_connection);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        Button buttonURLsettings = (Button) view.findViewById(R.id.button_url_settings);
+        Button buttonInvitePartner = (Button) view.findViewById(R.id.button_invite_partner);
+        Button buttonAccessVideoCall = (Button) view.findViewById(R.id.button_access_videocall);
+        Button buttonTestControls = (Button) view.findViewById(R.id.button_test_controls);
+
+        buttonTestControls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickTestControlls();
+            }
+        });
+    }
+
+    private void onClickTestControlls(){
+        FragmentManager fragmentManager = getParentFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, TestRobotFragment.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack(null) // name can be null
+                .commit();
+    }
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
