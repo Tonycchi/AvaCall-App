57a8e29ca644a829530699caf55262d11d708908
AlexanderGottfried <gottfried-alexander@gmx.net>
2021-05-10 12:04:32 +0200

    EditControlFragment

---
+++ app/src/main/java/com/example/rcvc/EditControlsFragment.java
@@ -0,0 +1,19 @@
+package com.example.rcvc;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class EditControlsFragment extends Fragment {
+
+    public EditControlsFragment() {
+        super(R.layout.edit_controls);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
