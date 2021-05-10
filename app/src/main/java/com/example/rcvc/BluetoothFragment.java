af101c1cb4e45edff97e02f7108ba48f7f861165
BenediktMehl <b.mehl14@outlook.de>
2021-05-10 13:08:32 +0200

    prepared fragment switch

--- app/src/main/java/com/example/rcvc/BluetoothFragment.java
+++ app/src/main/java/com/example/rcvc/BluetoothFragment.java
@@ -2,18 +2,39 @@ package com.example.rcvc;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class BluetoothFragment extends ConnectionFragment {
+
+
     public BluetoothFragment() {
         super(R.layout.bluetooth_connection);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        Button button_first_connection = (Button) view.findViewById(R.id.button_first_connection);
+        button_first_connection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickFirstBluetoothConnection();
+            }
+        });
+    }
+
+    private void onClickFirstBluetoothConnection(){
+        FragmentManager fragmentManager = getParentFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, .class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("name") // name can be null
+                .commit();
 
     }
+
 
 }
