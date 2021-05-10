252473b9ea313e2973d04a692b3a6e8801e4560b
Tonycchi <tn1499@yahoo.de>
2021-05-10 13:15:20 +0200

    fix host.xml (deleted FrameLayout)

--- app/src/main/java/com/example/rcvc/ModelSelectionFragment.java
+++ app/src/main/java/com/example/rcvc/ModelSelectionFragment.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ModelFragment extends Fragment {
+public class ModelSelectionFragment extends Fragment {
 
-    public ModelFragment() {
+    public ModelSelectionFragment() {
         super(R.layout.model_selection);
     }
     
