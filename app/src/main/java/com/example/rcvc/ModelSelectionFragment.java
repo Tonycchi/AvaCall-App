62d2875f095886cef4ca1e23870b7d4d870e7783
Tonycchi <tn1499@yahoo.de>
2021-05-10 13:28:50 +0200

    added switch to EditControlFragment from ModelSelectionFragment

--- app/src/main/java/com/example/rcvc/ModelSelectionFragment.java
+++ app/src/main/java/com/example/rcvc/ModelSelectionFragment.java
@@ -1,18 +1,41 @@
 package com.example.rcvc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class ModelSelectionFragment extends Fragment {
 
     public ModelSelectionFragment() {
         super(R.layout.model_selection);
     }
-    
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button useModel = view.findViewById(R.id.button_use_model);
+        Button editModel = view.findViewById(R.id.button_edit_model);
+
+        editModel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void onClickUseModel(View view) {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, EditControlsFragment.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack(null)
+                .commit();
     }
 }
