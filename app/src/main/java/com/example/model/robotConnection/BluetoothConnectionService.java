fba037476bc732a7b2f4641f8bb28fed9f89d4ad
Tonycchi <tn1499@yahoo.de>
2021-05-20 17:16:10 +0200

    added postValue(1) when connected

--- app/src/main/java/com/example/model/robotConnection/BluetoothConnectionService.java
+++ app/src/main/java/com/example/model/robotConnection/BluetoothConnectionService.java
@@ -144,6 +144,7 @@ public class BluetoothConnectionService {
                 }
             }
             connected(bluetoothSocket);
+            connectionStatus.postValue(1);
         }
 
         /**
