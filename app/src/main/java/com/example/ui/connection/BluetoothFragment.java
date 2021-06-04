a987150b5ed0459bd075a5d531a56a86ed672f17
Benedikt <b.mehl14@outlook.de>
2021-06-04 12:56:31 +0200

    onConfigurationChanged in HostedFragment now

--- app/src/main/java/com/example/ui/connection/BluetoothFragment.java
+++ app/src/main/java/com/example/ui/connection/BluetoothFragment.java
@@ -78,26 +78,6 @@ public class BluetoothFragment extends RobotConnectionFragment {
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        // Checks the orientation of the screen
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            getParentFragmentManager()
-                    .beginTransaction()
-                    .detach(this)
-                    .attach(this)
-                    .commit();
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            getParentFragmentManager()
-                    .beginTransaction()
-                    .detach(this)
-                    .attach(this)
-                    .commit();
-        }
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
 
