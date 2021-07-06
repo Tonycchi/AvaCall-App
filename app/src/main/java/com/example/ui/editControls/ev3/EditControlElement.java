895cc1f31598d37bec97255bb5f09e590921bf84
AlexanderGottfried <gottfried-alexander@gmx.net>
2021-07-06 16:10:30 +0200

    added layout/fragment files

---
+++ app/src/main/java/com/example/ui/editControls/ev3/EditControlElement.java
@@ -0,0 +1,6 @@
+package com.example.ui.editControls.ev3;
+
+import androidx.fragment.app.Fragment;
+
+public abstract class EditControlElement extends Fragment {
+}
---
+++ app/src/main/java/com/example/ui/editControls/ev3/EditJoystick.java
@@ -0,0 +1,23 @@
+package com.example.ui.editControls.ev3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.example.rcvc.R;
+
+public class EditJoystick extends EditControlElement {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle saveInstanceState) {
+        return inflater.inflate(R.layout.ev3_joystick, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+    }
+}
