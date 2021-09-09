ffc4c0044b3361b26decf013f42fc8f4046317fc
AlexanderGottfried <gottfried-alexander@gmx.net>
2021-09-09 16:41:10 +0200

    selected images are scaled down & saved in app storage, doesn't need persisted permissions

--- app/src/main/java/com/example/ui/modelSelection/ModelSelectionFragment.java
+++ app/src/main/java/com/example/ui/modelSelection/ModelSelectionFragment.java
@@ -5,11 +5,9 @@ import static android.app.Activity.RESULT_OK;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -41,26 +39,23 @@ import com.example.ui.editControls.EditControlsFragment;
 import net.simonvt.numberpicker.NumberPicker;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
+@SuppressLint("LogNotTimber")
 public class ModelSelectionFragment extends HostedFragment {
 
     private static final String TAG = "ModelSelectionFragment";
+    private static final int TAKE_PICTURE_REQUEST_CODE = 1;
+    private static final int SELECT_PICTURE_REQUEST_CODE = 2;
     private NumberPicker modelPicker;
     private MainViewModel viewModel;
     private TextView modelDescription;
     private ImageView modelPicture;
-
     private Context context;
-
-    private Uri outputFileUri;
-
-    private static final int TAKE_PICTURE_REQUEST_CODE = 1;
-    private static final int SELECT_PICTURE_REQUEST_CODE = 2;
     private String currentPhotoPath;
 
     public ModelSelectionFragment() {
@@ -94,7 +89,7 @@ public class ModelSelectionFragment extends HostedFragment {
         modelPicker = view.findViewById(R.id.model_picker);
 
         String[] allRobotNames = viewModel.getAllRobotNames();
-        modelPicker.setMaxValue(allRobotNames.length-1);
+        modelPicker.setMaxValue(allRobotNames.length - 1);
         modelPicker.setMinValue(0);
         modelPicker.setDisplayedValues(allRobotNames);
         modelPicker.setValue(viewModel.getSelectedModelPosition());
@@ -111,31 +106,28 @@ public class ModelSelectionFragment extends HostedFragment {
         getActivity().setTitle(R.string.title_model_selection);
     }
 
-    private void loadNewImage(){
+    private void loadNewImage() {
         String takePicture = getResources().getString(R.string.take_picture);
         String selectPicture = getResources().getString(R.string.select_picture);
         String deletePicture = getResources().getString(R.string.delete_picture);
         final CharSequence[] optionsMenu = {takePicture, selectPicture, deletePicture};
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setItems(optionsMenu, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if(optionsMenu[which].equals(takePicture)){
-                    takePicture();
-                }else if(optionsMenu[which].equals(selectPicture)){
-                    selectPicture();
-                }else if(optionsMenu[which].equals(deletePicture)){
-                    deletePicture();
-                }else{
-                    Log.e(TAG, "This can't happen - I hope lel");
-                }
+        builder.setItems(optionsMenu, (dialog, which) -> {
+            if (optionsMenu[which].equals(takePicture)) {
+                takePicture();
+            } else if (optionsMenu[which].equals(selectPicture)) {
+                selectPicture();
+            } else if (optionsMenu[which].equals(deletePicture)) {
+                deletePicture();
+            } else {
+                Log.e(TAG, "This can't happen - I hope lel");
             }
         });
         builder.show();
     }
 
-    private void takePicture(){
-        if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+    private void takePicture() {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             // Ensure that there's a camera activity to handle the intent
             if (takePictureIntent.resolveActivity(context.getPackageManager()) != null) {
@@ -154,33 +146,45 @@ public class ModelSelectionFragment extends HostedFragment {
                     final int takeFlags = Intent.FLAG_GRANT_READ_URI_PERMISSION
                             | Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
                     // Check for the freshest data.
-                    //getActivity().getContentResolver().takePersistableUriPermission(photoURI, takeFlags);
+                    getActivity().getContentResolver().takePersistableUriPermission(photoURI, takeFlags);
                     takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
                     startActivityForResult(takePictureIntent, TAKE_PICTURE_REQUEST_CODE);
-                }else{
+                } else {
                     Log.e(TAG, "photoFile == null");
                     failedTakingPicture();
                 }
-            }else{
+            } else {
                 Log.e(TAG, "takePictureIntent.resolveActivity(context.getPackageManager()) == null");
                 failedTakingPicture();
             }
-        }else{
+        } else {
             Log.e(TAG, "No camera feature!");
             failedTakingPicture();
         }
     }
-    private void selectPicture(){
+
+    private void selectPicture() {
         Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(pickPhoto, SELECT_PICTURE_REQUEST_CODE);
     }
-    private void deletePicture(){
+
+    private void deletePicture() {
+        RobotModel robotModel = viewModel.getRobotModel(modelPicker.getValue());
+        File img = new File(robotModel.picture);
+        if (!img.delete())
+            try {
+                if (img.getCanonicalFile().delete())
+                    getContext().deleteFile(img.getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         viewModel.setImageOfSelectedModel(null);
         updateModelPicture();
     }
 
-    private void storeTakenPicture(){
+    private void storeTakenPicture() {
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File photoFile = new File(currentPhotoPath);
         Log.d(TAG, "test");
@@ -192,7 +196,7 @@ public class ModelSelectionFragment extends HostedFragment {
         getActivity().sendBroadcast(mediaScanIntent);
     }
 
-    private void failedTakingPicture(){
+    private void failedTakingPicture() {
         ((HostActivity) getActivity()).showToast(getResources().getString(R.string.error_taking_picture));
     }
 
@@ -201,36 +205,79 @@ public class ModelSelectionFragment extends HostedFragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_OK) {
+        if (resultCode == RESULT_OK) {
             if (requestCode == TAKE_PICTURE_REQUEST_CODE) {
                 storeTakenPicture();
                 viewModel.setImageOfSelectedModel(currentPhotoPath);
                 updateModelPicture();
             } else if (requestCode == SELECT_PICTURE_REQUEST_CODE) {
                 Uri selectedImageUri = null;
-                if(data != null) {
+                if (data != null) {
                     selectedImageUri = data.getData();
-                }else{
+                } else {
                     ((HostActivity) getActivity()).showToast(getResources().getString(R.string.error_selecting_picture));
-                    Log.e(TAG,"data==null");
+                    Log.e(TAG, "data==null");
                 }
 
-                final int takeFlags = data.getFlags()
-                        & (Intent.FLAG_GRANT_READ_URI_PERMISSION
-                        | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                // Check for the freshest data.
-                getActivity().getContentResolver().takePersistableUriPermission(selectedImageUri, takeFlags);
-
+                Uri savedImg = saveScaledDown(selectedImageUri);
+                currentPhotoPath = savedImg.toString();
                 viewModel.setImageOfSelectedModel(currentPhotoPath);
                 updateModelPicture();
             }
-        }else{
+        } else {
             Log.e(TAG, "resultCode != RESULT_OK");
         }
     }
 
+    /**
+     * save a scaled down copy of selected image in app specific storage
+     * @param imageUri uri of selected image
+     * @return uri of scaled down copy
+     */
+    private Uri saveScaledDown(Uri imageUri) {
+        // get bitmap from uri
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), imageUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // scale down to 480p (for now)
+        Bitmap resizedBitmap;
+        int maxSize = 480;
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            resizedBitmap = Bitmap.createScaledBitmap(bitmap, maxSize, maxSize * bitmap.getHeight() / bitmap.getWidth(), false);
+        } else {
+            resizedBitmap = Bitmap.createScaledBitmap(bitmap, maxSize * bitmap.getWidth() / bitmap.getHeight(), maxSize, false);
+        }
+
+        // get app private directory
+        File dir = context.getDir("imageDir", Context.MODE_PRIVATE);
+        // create directory for new image file
+        File imgPath = new File(dir, new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.GERMANY).format(new Date()) + ".jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(imgPath);
+            // save as jpg
+            resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 95, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return Uri.fromFile(imgPath);
+    }
+
     private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.GERMANY).format(new Date());
         String imageFileName = "ModelPicture_" + timeStamp;
         File imageDir = getImageDir();
         File imageFile = File.createTempFile(imageFileName, ".jpg", imageDir);
@@ -239,48 +286,24 @@ public class ModelSelectionFragment extends HostedFragment {
         return imageFile;
     }
 
-    private File getImageDir(){
+    private File getImageDir() {
         //File imageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         File imageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        Log.d(TAG,"1imagedir:"+imageDir.getAbsolutePath());
+        Log.d(TAG, "1imagedir:" + imageDir.getAbsolutePath());
         if (!imageDir.exists()) {
             imageDir.mkdirs();
         }
-        Log.d(TAG,"2imagedir:"+imageDir.getAbsolutePath());
+        Log.d(TAG, "2imagedir:" + imageDir.getAbsolutePath());
         return imageDir;
     }
 
     private void updateModelPicture() {
         RobotModel robotModel = viewModel.getRobotModel(modelPicker.getValue());
-        if(robotModel.picture == null) {
+        if (robotModel.picture == null) {
             modelPicture.setImageResource(R.drawable.no_image_available);
             Log.d(TAG, "default pic");
         } else {
-            Log.d(TAG,"pic:"+robotModel.picture);
-            // Get the dimensions of the View
-            int targetW = modelPicture.getWidth() == 0 ? 1 : modelPicture.getWidth();
-            int targetH = modelPicture.getHeight() == 0 ? 1 : modelPicture.getHeight();
-
-            // Get the dimensions of the bitmap
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-
-            String robotModelPicturePath = robotModel.picture;
-            BitmapFactory.decodeFile(robotModelPicturePath, bmOptions);
-
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
-
-            // Determine how much to scale down the image
-            int scaleFactor = Math.max(1, Math.min(photoW / targetW, photoH / targetH));
-
-            // Decode the image file into a Bitmap sized to fill the View
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
-            bmOptions.inPurgeable = true;
-
-            Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-            modelPicture.setImageBitmap(bitmap);
+            modelPicture.setImageURI(Uri.parse(robotModel.picture));
         }
     }
 
@@ -342,18 +365,16 @@ public class ModelSelectionFragment extends HostedFragment {
 
     private void onSelectedModelChanged(NumberPicker modelPicker, int oldVal, int newVal) {
         viewModel.setSelectedModelPosition(newVal);
-        RobotModel robotModel = viewModel.getRobotModel(newVal);
         setModelDescription();
         updateModelPicture();
-        //Log.d(TAG, "Model at Position "+newVal+" selected! Name:"+robotModel.name+" Id:"+robotModel.id);
     }
 
-    private void setModelDescription(){
+    private void setModelDescription() {
         RobotModel robotModel = viewModel.getRobotModel(modelPicker.getValue());
         String descriptionText = robotModel.description;
-        if(descriptionText==null || descriptionText.isEmpty())
+        if (descriptionText == null || descriptionText.isEmpty())
             descriptionText = robotModel.specs;
-        modelDescription.setText(robotModel.name+"("+robotModel.type+"): "+descriptionText);
+        modelDescription.setText(robotModel.name + "(" + robotModel.type + "): " + descriptionText);
     }
 
     private void onClickEditModel(View v) {
@@ -383,16 +404,12 @@ public class ModelSelectionFragment extends HostedFragment {
 
     @Override
     public void connectionStatusChanged(Integer newConnectionStatus) {
-         //0 is not tested, 1 is connected, 2 is could not connect, 3 is connection lost, 4 connection is accepted = correct device, 5 connection is not accepted = wrong device
-        switch (newConnectionStatus) {
-            case 3:
-                Log.d(TAG, "Case 3: Connection lost!");
-                ((HostActivity) getActivity()).showToast(getResources().getString(R.string.connection_lost));
-                break;
-
-            default:
-                Log.d(TAG, "Default: Something strange or nothing(Case -1) happend with the connection.");
-                break;
+        //0 is not tested, 1 is connected, 2 is could not connect, 3 is connection lost, 4 connection is accepted = correct device, 5 connection is not accepted = wrong device
+        if (newConnectionStatus == 3) {
+            Log.d(TAG, "Case 3: Connection lost!");
+            ((HostActivity) getActivity()).showToast(getResources().getString(R.string.connection_lost));
+        } else {
+            Log.d(TAG, "Default: Something strange or nothing(Case -1) happend with the connection.");
         }
 
     }
